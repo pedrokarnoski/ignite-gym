9ca6c8a8c4190f3081fe20ec03e56f327d93e67a
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-05-02 20:03:51 -0300

    feat: Typing routes

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,15 +1,25 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
 import { SignIn } from "@/pages/SignIn";
 import { SignUp } from "@/pages/SignUp";
 
+type AuthRoutes = {
+  signIn: undefined;
+  signUp: undefined;
+};
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
 export function AuthRoutes() {
   return (
     <Navigator>
-      <Screen name="SignIn" component={SignIn} />
-      <Screen name="SignUp" component={SignUp} />
+      <Screen name="signIn" component={SignIn} />
+      <Screen name="signUp" component={SignUp} />
     </Navigator>
   );
 }
